4c5462e00d4b17b8d9a4133d935aa9dc981454c8
Komal Kotyal <komal.wce@gmail.com>
2017-01-15 14:34:06 +0530

    Added Team Model

---
+++ db/migrate/20170115084337_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration[5.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :topic_name
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
