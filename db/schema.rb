1444f34e13123307f1d61bd62683e60631615d14
Komal Kotyal <komal.wce@gmail.com>
2017-01-15 15:45:35 +0530

    Association Added

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115092912) do
+ActiveRecord::Schema.define(version: 20170115100930) do
 
   create_table "members", force: :cascade do |t|
     t.string   "name"
     t.string   "college"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "team_id"
+    t.index ["team_id"], name: "index_members_on_team_id"
   end
 
   create_table "teams", force: :cascade do |t|
